a3adc47a2b0f5243927bca75cb6a3b5b3e927f4e
Anatoly <bmth@me.com>
2021-04-27 00:42:38 +0300

    Update middlewares.py

--- scrapy_selenium_seleniumwire/middlewares.py
+++ scrapy_selenium_seleniumwire/middlewares.py
@@ -43,14 +43,16 @@ class SeleniumMiddleware:
 
         driver_kwargs = {
             'executable_path': driver_executable_path,
-            f'{driver_name}_options': driver_options
+            f'{driver_name}_options': driver_options,
+            'seleniumwire_options:' {'connection_timeout': 15}
         }
 
         # locally installed driver
         if driver_executable_path is not None:
             driver_kwargs = {
                 'executable_path': driver_executable_path,
-                f'{driver_name}_options': driver_options
+                f'{driver_name}_options': driver_options,
+                'seleniumwire_options': {'connection_timeout': 15}
             }
             self.driver = driver_klass(**driver_kwargs)
         # remote driver
